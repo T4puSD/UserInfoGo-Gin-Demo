af0a2c6c612605de93995dde57d5458ec608639c
t4pusd <sdtapusd20@gmail.com>
2022-07-19 16:32:12 +0600

    implemented update user endpoint

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -88,3 +88,65 @@ func CreateUser() gin.HandlerFunc {
 		})
 	}
 }
+
+func UpdateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		// validate request body
+		var user model.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{
+				Status:  http.StatusBadRequest,
+				Message: "error",
+				Data:    map[string]interface{}{"data": err.Error()},
+			})
+			return
+		}
+
+		// validate with the validator library
+		if err := validate.Struct(&user); err != nil {
+			c.JSON(http.StatusBadRequest, responses.UserResponse{
+				Status:  http.StatusBadRequest,
+				Message: "error",
+				Data:    map[string]interface{}{"data": err.Error()},
+			})
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		objectId, _ := primitive.ObjectIDFromHex(id)
+
+		update := bson.M{"name": user.Name, "email": user.Email}
+		result, err := userCollection.UpdateOne(ctx, bson.M{"id": objectId}, bson.M{"$set": update})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.UserResponse{
+				Status:  http.StatusInternalServerError,
+				Message: "error",
+				Data:    map[string]interface{}{"data": err.Error()},
+			})
+			return
+		}
+
+		var updatedUser model.User
+
+		if result.MatchedCount == 1 {
+			err := userCollection.FindOne(ctx, bson.M{"id": objectId}).Decode(&updatedUser)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, responses.UserResponse{
+					Status:  http.StatusInternalServerError,
+					Message: "error",
+					Data:    map[string]interface{}{"data": err.Error()},
+				})
+				return
+			}
+		}
+
+		c.JSON(http.StatusOK, responses.UserResponse{
+			Status:  http.StatusOK,
+			Message: "success",
+			Data:    map[string]interface{}{"data": updatedUser},
+		})
+	}
+}
--- routes/user_route.go
+++ routes/user_route.go
@@ -8,4 +8,5 @@ import (
 func UserRoutes(route *gin.Engine) {
 	route.GET("/users/:id", controllers.GetUser())
 	route.POST("/users", controllers.CreateUser())
+	route.PUT("/users/:id", controllers.UpdateUser())
 }
